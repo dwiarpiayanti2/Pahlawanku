7c88a0198204a05bb1e54cab2da44728d40d2e78
dheaanisah <anisahdhiyaa@gmail.com>
2022-11-16 15:13:27 +0700

    Upload Adapter Card

--- app/src/main/java/dea/if3b/myapplication/AdapterCard.java
+++ app/src/main/java/dea/if3b/myapplication/AdapterCard.java
@@ -1,21 +1,56 @@
 package dea.if3b.myapplication;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class AdapterCard {
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class AdapterCard extends RecyclerView.Adapter<AdapterCard.ClassViewHolder> {
+     private ArrayList<ModelPahlawan> dataPahlawan;
+
+    public AdapterCard(ArrayList<ModelPahlawan> dataPahlawan) {
+        this.dataPahlawan = dataPahlawan;
+    }
+
+    @NonNull
+    @Override
+    public ClassViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View varView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card, parent, false);
+        return new ClassViewHolder(varView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ClassViewHolder holder, int position) {
+       ModelPahlawan pahlawan = dataPahlawan.get(position);
+       holder.tvNama.setText(pahlawan.getNama());
+       holder.tvTentang.setText(pahlawan.getTentang());
+        Glide
+                .with(holder.itemView.getContext())
+                .load(pahlawan.getFoto())
+                .centerCrop()
+                .into(holder.ivFoto);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataPahlawan.size();
+    }
 
     public class ClassViewHolder extends RecyclerView.ViewHolder {
-       ImageView ivfoto;
+       ImageView ivFoto;
        TextView tvNama, tvTentang;
 
         public ClassViewHolder(@NonNull View itemView) {
             super(itemView);
-            ivfoto = itemView.findViewById(R.id.iv_foto);
+            ivFoto = itemView.findViewById(R.id.iv_foto);
             tvNama = itemView.findViewById(R.id.tv_nama);
             tvTentang = itemView.findViewById(R.id.tv_tentang);
         }
